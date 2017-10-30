a3961583139b7cbcc55ebbe173b37c2178ea46eb
Michael Käufl <michael-k@users.noreply.github.com>
2017-10-30 22:51:16 +0100

    Enforce secure cookies

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ UID=${UID:-991}
 
 chown $UID:$GID /roundcubemail/temp /roundcubemail/logs
 sed -i "s#;date.timezone =#date.timezone = \"${TIMEZONE:-UTC}\"#" /etc/php7/php.ini
+sed -i "s#;session.cookie_secure =#session.cookie_secure = True#" /etc/php7/php.ini
 
 exec su-exec $UID:$GID php7 -S 0.0.0.0:8888 -t /roundcubemail
